8b2e2ecd9318c6f8bb8708ef1ae3e9c367027a6c
ndolb21 <ndolb21@freeuni.edu.ge>
2025-05-16 00:08:15 +0400

    added Customer Controller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bankOfLeverx.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : Controller
+    {
+        private  readonly ILogger<CustomersController> _logger;
+
+        private  static int currentKey = 1000;
+
+        private static  List<Customer> Customers = new List<Customer>();    
+
+
+        public CustomersController(ILogger<CustomersController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        [HttpGet(Name ="GetCustomers")]
+        public IEnumerable<Customer> get()
+        {
+            return Customers;
+        }
+
+        [HttpGet("{customerKey}", Name = "GetCustomer")]
+        public ActionResult<Customer> Get(int customerKey)
+        {
+            var customer = Customers.FirstOrDefault(e => e.Key == customerKey);
+            if (customer == null)
+            {
+                return NotFound($"Customer with Key {customerKey} not found.");
+            }
+            return Ok(customer);
+        }
+
+        [HttpPost(Name = "PostCustomer")]
+        public ActionResult<Customer> Post([FromBody] CustomerDTO customer)
+        { 
+            
+            Customer cust = new Customer
+            {
+                Key = currentKey++,
+                Name = customer.Name,
+                Surname = customer.Surname,
+                Category = customer.Category,
+                Branch = customer.Branch
+            };
+            Customers.Add(cust);
+            return Ok(customer);
+        }
+        [HttpPatch("{customerKey}",Name ="PatchCustomer")] 
+        public ActionResult<Customer> patch(int customerKey, [FromBody] CustomerPatchDto customer)
+        {
+            Customer cust = Customers.FirstOrDefault(e => e.Key == customerKey);
+            if (cust == null)
+            {
+                return NotFound($"Customer with Key {customerKey} not found."); 
+            }
+            if (customer.Name != null)
+            {
+                cust.Name = customer.Name;
+            }
+            if (customer.Surname != null)
+            {
+                cust.Surname = customer.Surname;
+            }
+            if (customer.Category != null)
+            {
+                cust.Category = customer.Category;
+            }
+            if (customer.Branch != null)
+            {
+                cust.Branch = customer.Branch;
+            }
+            return Ok(cust);
+
+        }
+        
+    }
+
+}
---
+++ Customer.cs
@@ -0,0 +1,14 @@
+namespace bankOfLeverx
+{
+    public class Customer
+    {
+        public required string Name { get; set; }
+
+        public required string Surname { get; set; }
+        public required int Key { get; set; }
+
+        public required string Category { get; set; }
+
+        public required string Branch { get; set; }
+    }
+}
